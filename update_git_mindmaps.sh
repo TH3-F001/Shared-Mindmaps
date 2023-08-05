f9d48d254aafd3dfaa48d0936ea05e602d35c575
f00l <thef00l@f00lsgold.com>
2023-08-05 16:15:46 -0700

    New commit from remote location: hanzo@fedora

--- update_git_mindmaps.sh
+++ update_git_mindmaps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git remote add origin git@github.com:TH3-F001/Shared-Mindmaps.git
 git add *
 git commit -m "New commit from remote location: ${USER}@${HOSTNAME}"
 git push origin main
